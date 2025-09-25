72a8bdba2fd8ae38b6d455fd8bd482dc268ddb3f
khaan <nguyenkhaan2006@gmail.com>
2025-09-26 06:50:34 +0700

    them_page_print

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -1,8 +1,13 @@
 import Home from "../Pages/Home"
+import Print from "../Pages/Print"
 const router = [
     {
         path: '/home', 
         element: Home 
     }, 
+    {
+        path: '/print', 
+        element: Print
+    }
 ] 
 export default router 
